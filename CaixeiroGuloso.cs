b9cb59e406c5a4e76a07e0c0bb135a0c7502bf89
BarbaraGCOL <barbara.gcol@gmail.com>
2015-06-22 19:51:42 -0300

    Implementação

---
+++ CaixeiroGuloso.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caixeiro
+{
+    class Rota
+    {
+        public int cidade1, cidade2, custo;
+    }
+
+    //-----------------------------------------------------------------------------
+
+    /* matriz que contera' o grafo completo */
+    class Grafo
+    {
+        public int[,] M; /** o grafo esperado*/
+    }
+
+    //-----------------------------------------------------------------------------
+
+    class CaixeiroGuloso
+    {
+        const int MaxCusto = 50;// custo ou distância máxima entre duas cidades
+
+        /**
+         * Monta a rota obtida através do algortimo guloso
+        */
+        void encontraCaminho(Grafo grafo, List<Rota> caminho, ref int custo)
+        {
+            int i,cid1=0,cid2=0,menorCusto,tamanho=caminho.Capacity;
+            int[] pos = new int[grafo.M.Length];
+            List<int>adicionados=new List<int>(grafo.M.Length);//Lista contendo os vértices que já foram adicionados à rota
+            adicionados.Add(0);//adiciona o primeiro vértice (0)
+            Rota rota = new Rota();
+            custo = 0;
+
+            for (i = 0; i < tamanho;i++)
+            {
+                menorCusto = MaxCusto + 1;
+                rota.cidade1 = cid1;//Adiciona a cidade 1 à rota
+
+                //No final retorna ao vértice inicial (0)
+                if(i==caminho.Capacity-1)
+                {
+                    menorCusto = grafo.M[cid1,0];
+                    cid2 = 0;
+                }
+                else
+                {
+                    for (int j = 0; j < tamanho; j++)
+                    {
+                        if (j != cid1 && !adicionados.Contains(j))
+                        {
+                            //Encontra o arco adjacente com menor peso, e o vértice adjacente a ele
+                            if (grafo.M[cid1,j] != 0 && grafo.M[cid1,j] < menorCusto )
+                            {
+                                menorCusto = grafo.M[cid1,j];
+                                cid2 = j;
+                            }
+                        }
+                    }
+                }
+                rota.cidade2 = cid2;//adiciona a segunda cidade à rota()
+                rota.custo = menorCusto;//adiciona o custo da rota
+                adicionados.Add(cid2);
+                custo += menorCusto;//adiciona o custo da rota a soma do custo total
+                caminho.Add(rota);//Adiciona a rota oa caminho
+                rota = new Rota();
+                cid1 = cid2;
+            }
+        }
+
+        void montaGrafo(out Grafo grafo, int numCidades)
+        {
+            int custo;          // pesos dos arcos {i, j}
+            
+            Random randomizer = new Random();
+            grafo = new Grafo();
+            grafo.M = new int[numCidades, numCidades];
+
+            for (int i = 0; i < numCidades; i++)
+            {
+                for (int j = 0; j < numCidades; j++)
+                {
+                    custo = randomizer.Next(MaxCusto) + 1;
+                    if (i < j)
+                        grafo.M[i, j] = custo;
+                    else
+                        if (i == j)
+                            grafo.M[i, j] = 0;
+                        else
+                            grafo.M[i, j] = grafo.M[j, i];
+                }
+            }
+
+            /* Para conferência: no formatting at all!!! */
+            Console.Write("\nCidades e custos:\n   ");
+            for (int i = 0; i < numCidades; i++)   // preenche a matriz grafo -> M
+                Console.Write(i + " ");
+            Console.WriteLine();
+            for (int i = 0; i < numCidades; i++)
+            { 
+                // preenche a matriz grafo -> M
+                Console.Write(i + " ");
+                for (int j = 0; j < numCidades; j++)
+                {
+                    Console.Write(" " + grafo.M[i, j]);
+                }
+                Console.WriteLine();
+            }
+        } // fim montaGrafo
+
+        /**
+         * Imprime caminho encontrato pelo algoritmo guloso
+         */
+        void imprimeCaminho(int custo, List<Rota> melhorRota)
+        {
+            Console.WriteLine("\n\nCUSTO MINIMO PARA A VIAGEM DO CAIXEIRO: " + custo);
+            Console.WriteLine("\n\nMELHOR CAMINHO PARA A VIAGEM DO CAIXEIRO:");
+            Console.WriteLine("\n\n              DE               PARA             CUSTO ");
+            foreach(Rota rota in melhorRota)
+            {
+                Console.Write("              " + rota.cidade1 + "                  " + rota.cidade2 + "                " + rota.custo + "\n");
+            }
+            Console.WriteLine("\n");
+        } /* fim ImprimeCaminho */
+
+        /**
+         * Imprime o tempo gasto na execução do algoritmo
+         */
+        void imprimeTempo(Stopwatch tempo)
+        {
+            Console.WriteLine("TEMPO DE EXECUÇÂO: ");
+            Console.WriteLine(tempo.Elapsed.Hours + " horas " + tempo.Elapsed.Minutes + " minutos " + tempo.Elapsed.Seconds + " segundos " + tempo.Elapsed.Milliseconds + " milisegundos");
+        } /* fim Imprime tempo */
+
+        static void Main(string[] args)
+        {
+            List<Rota> caminho;    /* conterá a rota da viagem */
+            int numCidades,      /* numero de vértices (cidades) do grafo */
+                    custo = 0;     /* custo da viagem pelo grafo (pelas cidades) */
+            CaixeiroGuloso caixeiro = new CaixeiroGuloso();
+            Grafo grafo; // = new Grafo();         /* matriz de adjacencia com o grafo */
+
+            Console.Write("Digite o número de cidades e tecle ENTER: ");
+            numCidades = int.Parse(Console.ReadLine());
+            caixeiro.montaGrafo(out grafo, numCidades);
+
+            caminho = new List<Rota>(numCidades);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();//Inicia a contagem do tempo
+            caixeiro.encontraCaminho(grafo, caminho,ref custo);
+            caixeiro.imprimeCaminho(custo, caminho);
+            stopwatch.Stop();//Encerra a contagem do tempo
+            caixeiro.imprimeTempo(stopwatch);
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ CaixeiroOtimo.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Caixeiro
+{
+
+    /* Um campo do vetor que contera a melhor rota */
+    class Rota
+    {
+        public int cidade1, cidade2, custo;
+    }
+
+    //-----------------------------------------------------------------------------
+
+    /* matriz que contera' o grafo completo */
+    class Grafo
+    {
+        public int[,] M; /** o grafo esperado*/
+    }
+
+    //-----------------------------------------------------------------------------
+
+    class CaixeiroOtimo
+    {
+        const int MaxCusto = 50;     // custo ou distância máxima entre duas cidades
+        //------------------------------------------------------------------------------
+
+        /* 
+         * Verifica se a permutação passada como parâmetro tem custo melhor que o custo
+         * já obtido. Caso positivo, então monta a rota correspondnete à permutação como
+         * sendo a melhor rota (e armazena no vetor melhorRota, retornando tambem o custo 
+         * total da melhor rota 
+         */
+        void melhorCaminho(Grafo grafo, Rota[] melhorRota, ref int melhorCusto, int[] permutacao)
+        {
+            int j, k;                     /* contadores: auxiliam a montagem das rotas */
+            int cid1, cid2;             /* cidades da melhor rota */
+            int custo;                 /* custo total da melhor rota */
+            int[] proxDaRota;        /* vetor que armazena a sequencia de cidades que estao
+				                           em uma rota, tal que um indice indica uma cidade e
+				                           o conteudo deste indice, a proxima cidade da rota */
+
+            proxDaRota = new int[melhorRota.Length];
+            /* monta uma rota com a permutacao */
+            cid1 = 0;									/* a primeira cidade é a cidade 0 */
+            cid2 = permutacao[1];
+            custo = grafo.M[cid1, cid2];
+            proxDaRota[cid1] = cid2;
+
+            for (j = 2; j < melhorRota.Length; j++)
+            {
+                cid1 = cid2;
+                cid2 = permutacao[j];
+                custo += grafo.M[cid1, cid2];  /* calcula o custo parcial da rota */
+                proxDaRota[cid1] = cid2;      /* armazena a rota fornecida pela permutacao */
+            }
+
+            proxDaRota[cid2] = 0;			/* completa o ciclo da viagem */
+            custo += grafo.M[cid2, 0];  /* custo total desta rota */
+
+            if (custo < melhorCusto)	/* procura pelo melhor (menor) custo */
+            {
+                melhorCusto = custo;
+                cid2 = 0;
+                for (k = 0; k < melhorRota.Length; k++) /* guarda a melhor rota */
+                {
+                    cid1 = cid2;
+                    cid2 = proxDaRota[cid1];
+                    melhorRota[k].cidade1 = cid1;
+                    melhorRota[k].cidade2 = cid2;
+                    melhorRota[k].custo = grafo.M[cid1, cid2];
+                }
+            }
+        } /* fim melhorCaminho */
+
+        //------------------------------------------------------------------------------
+        /* Gera os possiveis caminhos entre a cidade zero e as outras (N-1) envolvidas
+             na busca, armazenando-os no vetor permutacao, um por vez, e a cada permutacao
+             gerada, chama a funcao melhorCaminho que escolhe o caminho (a permutacao) de
+             menor custo.
+         * CÓDIGO ADAPTADO DE "Algorithms in C" (Robert Sedgewick), página 624.
+        */
+        void permuta(int[] permutacao, Grafo grafo, Rota[] melhorRota, ref  int melhorCusto,int controle, int k)
+        {
+            int i;
+            permutacao[k] = ++controle;
+            if (controle == (melhorRota.Length - 1)) /* se gerou um caminho então verifica se ele é melhor */
+                melhorCaminho(grafo, melhorRota, ref melhorCusto, permutacao);
+            else
+                for (i = 1; i < melhorRota.Length; i++)
+                    if (permutacao[i] == 0)
+                        permuta(permutacao, grafo, melhorRota, ref melhorCusto, controle, i);
+            controle--;
+            permutacao[k] = 0;
+        } /* fim permuta */
+
+        //------------------------------------------------------------------------------
+
+        /*  Gera os pesos dos arcos do grafo randomicamente e preenche
+           a matriz grafo->M, que e indexada pelos nomes dos vertices (cidades)
+         */
+        void montaGrafo(out Grafo grafo, int numCidades)
+        {
+            //int i, j;      // indexadores da matriz
+            int custo;             // pesos dos arcos {i, j}
+
+            Random randomizer = new Random();
+            grafo = new Grafo();
+            grafo.M = new int[numCidades, numCidades];
+
+            for (int i = 0; i < numCidades; i++)
+            {
+                for (int j = 0; j < numCidades; j++)
+                {
+                    custo = randomizer.Next(MaxCusto) + 1;
+                    if (i < j)
+                        grafo.M[i, j] = custo;
+                    else
+                        if (i == j)
+                            grafo.M[i, j] = 0;
+                        else
+                            grafo.M[i, j] = grafo.M[j, i];
+                }
+            }
+
+            /* Para conferência: no formatting at all!!! */
+            Console.Write("\nCidades e custos:\n   ");
+            for (int i = 0; i < numCidades; i++)   // preenche a matriz grafo -> M
+                Console.Write(i + " ");
+            Console.WriteLine();
+            for (int i = 0; i < numCidades; i++)
+            { // preenche a matriz grafo -> M
+                Console.Write(i + " ");
+                for (int j = 0; j < numCidades; j++) 
+                {
+                    Console.Write(" " + grafo.M[i, j]);
+                }
+                Console.WriteLine();
+            }
+        } // fim montaGrafo
+
+        //------------------------------------------------------------------------------
+        /*
+                Gera os possiveis caminhos entre a cidade zero e todas as outras envolvidas
+             na rota da viagem do caixeiro e escolhe a melhor rota entre todas.
+        */
+        void geraEscolheCaminhos(ref int[] permutacao, Grafo grafo, Rota[] melhorRota, out int melhorCusto)
+        {
+            int controle=-1;
+            melhorCusto = int.MaxValue;
+            
+            for (int i = 0; i < melhorRota.Length; i++)
+                melhorRota[i] = new Rota();
+            
+            /* Gera os caminhos possiveis e escolhe o melhor, chamando a funcao recursiva
+             permuta */
+            permuta(permutacao, grafo, melhorRota, ref melhorCusto, controle, 1);
+        } /* fim GeraEscolheCaminhos */
+
+        //------------------------------------------------------------------------------
+        /*
+            Imprime o melhor caminho para a viagem do caxeiro, bem como o custo total da
+            viagem.
+        */
+        void imprimeMelhorCaminho(int custo, Rota[] melhorRota)
+        {
+            int i; /* indexa o vetor que contem a rota */
+            Console.WriteLine("\n\nCUSTO MINIMO PARA A VIAGEM DO CAIXEIRO: " + custo);
+            Console.WriteLine("\n\nMELHOR CAMINHO PARA A VIAGEM DO CAIXEIRO:");
+            Console.WriteLine("\n\n              DE               PARA             CUSTO ");
+            for (i = 0; i < melhorRota.Length; i++)
+            {
+                Console.Write("              " + melhorRota[i].cidade1 + "                  " + melhorRota[i].cidade2 + "                " + melhorRota[i].custo +"\n");
+            }
+            Console.WriteLine("\n");
+        } /* fim ImprimeMelhorCaminho */
+
+        //------------------------------------------------------------------------------
+
+        void imprimeTempo(Stopwatch tempo)
+        {
+            Console.WriteLine("TEMPO DE EXECUÇÂO: ");
+            Console.WriteLine(tempo.Elapsed.Hours+" horas "+tempo.Elapsed.Minutes+" minutos "+tempo.Elapsed.Seconds+" segundos "+tempo.Elapsed.Milliseconds+" milisegundos");
+        } /* fim Imprime tempo */
+
+        //------------------------------------------------------------------------------
+
+        
+        /* Coordena as partes do programa */
+        static void Main(string[] args)
+        {
+            int[] permutacao;     /* vetor com uma possivel rota de viagem */
+            Rota[] melhorRota;    /* contera' a melhor rota da viagem */
+            int numCidades,      /* numero de vertices (cidades) do grafo */
+                    melhorCusto;     /* custo da viagem pelo grafo (pelas cidades) */
+
+            CaixeiroOtimo caixeiro = new CaixeiroOtimo();
+            Grafo grafo; // = new Grafo();         /* matriz de adjacencia com o grafo */
+
+            Console.Write("\n\n\t\tDigite o numero de cidades: ");
+            numCidades = int.Parse(Console.ReadLine());
+            caixeiro.montaGrafo(out grafo, numCidades);
+
+            permutacao = new int[numCidades];
+            melhorRota = new Rota[numCidades];
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();//Inicia a contagem do tempo
+            caixeiro.geraEscolheCaminhos(ref permutacao, grafo, melhorRota, out melhorCusto);
+            stopwatch.Stop();//Encerra a contagem do tempo
+            caixeiro.imprimeMelhorCaminho(melhorCusto, melhorRota);
+            caixeiro.imprimeTempo(stopwatch);
+
+            Console.ReadKey(true);
+        } /* fim main */
+    }
+}
